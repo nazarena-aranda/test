3327150665283812f20c45b76ecf2c81455e6a38
SFerFer1 <10048@holbertonstudents.com>
2025-06-11 10:34:31 -0300

    mongo try

--- APIt/Resources/Models/User.cs
+++ APIt/Resources/Models/User.cs
@@ -10,17 +10,12 @@ namespace APIt.Resources.Models
         public string Documents { get; set; }
         public string[]? UserBiometric { get; set; }
 
-        public int FailedAttempts { get; set; }
-        public int SuccessfulAttempts { get; set; }
-
         public User(string? userId, string typeDocuments, string documents, string[]? userBiometric = null)
         {
             User_Id = userId;
             TypeDocuments = typeDocuments;
             Documents = documents;
             UserBiometric = userBiometric;
-            FailedAttempts = 0;
-            SuccessfulAttempts = 0;
         }
     }
 }
